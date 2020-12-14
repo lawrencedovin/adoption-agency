727db747e2592fb461a7a6e95fbcb9f0aa3cefd6
Lawrence Dovin <lawrencedovin@gmail.com>
2020-12-13 21:29:11 -0500

    Added Pet model and seed file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, request, render_template, redirect, flash, session
 from flask_debugtoolbar import DebugToolbarExtension
 from models import *
-from forms import AddSnackForm, EmployeeForm
+# from forms import AddSnackForm, EmployeeForm
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///employees_db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adoption_agency_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SECRET_KEY'] = 'chickenzarecool123'
@@ -14,71 +14,71 @@ debug = DebugToolbarExtension(app)
 
 connect_db(app)
 
-@app.route('/')
-def home_page():
-    return render_template('home.html')
+# @app.route('/')
+# def home_page():
+#     return render_template('home.html')
 
-@app.route('/phones')
-def list_phones():
-    employees = Employee.query.all()
-    return render_template('phones.html', employees=employees)
+# @app.route('/phones')
+# def list_phones():
+#     employees = Employee.query.all()
+#     return render_template('phones.html', employees=employees)
 
-@app.route('/snacks/new', methods=['GET', 'POST'])
-def add_snack():
-    """Snack add form; handle adding."""
+# @app.route('/snacks/new', methods=['GET', 'POST'])
+# def add_snack():
+#     """Snack add form; handle adding."""
 
-    form = AddSnackForm()
+#     form = AddSnackForm()
 
-    if form.validate_on_submit():
-        name = form.name.data
-        price = form.price.data
-        quantity = form.quantity.data
-        total_price = price * quantity
-        is_healthy = form.is_healthy.data
-        if is_healthy:
-            flash(f'Added {name} at ${price} each and a healthy snack. Quantity: {quantity}, total price: ${total_price}')
-        else: 
-            flash(f'Added {name} at ${price} and a junkfood snack. Quantity: {quantity}, total price: ${total_price}')
-        return redirect('/')
-    else:
-        return render_template('add_snack_form.html', form=form)
+#     if form.validate_on_submit():
+#         name = form.name.data
+#         price = form.price.data
+#         quantity = form.quantity.data
+#         total_price = price * quantity
+#         is_healthy = form.is_healthy.data
+#         if is_healthy:
+#             flash(f'Added {name} at ${price} each and a healthy snack. Quantity: {quantity}, total price: ${total_price}')
+#         else: 
+#             flash(f'Added {name} at ${price} and a junkfood snack. Quantity: {quantity}, total price: ${total_price}')
+#         return redirect('/')
+#     else:
+#         return render_template('add_snack_form.html', form=form)
 
-@app.route('/employees/new', methods=['GET', 'POST'])
-def add_employee():
+# @app.route('/employees/new', methods=['GET', 'POST'])
+# def add_employee():
 
-    form = EmployeeForm()
+#     form = EmployeeForm()
 
-    # Adds a tuple to the dept_code.choices select form field ie. ('mktg', 'Marketing')
-    depts = db.session.query(Department.dept_code, Department.dept_name)
-    form.dept_code.choices = depts
-    if form.validate_on_submit():
-        name = form.name.data
-        state = form.state.data
-        dept_code = form.dept_code.data
+#     # Adds a tuple to the dept_code.choices select form field ie. ('mktg', 'Marketing')
+#     depts = db.session.query(Department.dept_code, Department.dept_name)
+#     form.dept_code.choices = depts
+#     if form.validate_on_submit():
+#         name = form.name.data
+#         state = form.state.data
+#         dept_code = form.dept_code.data
 
-        new_employee = Employee(name=name, state=state, dept_code=dept_code)
-        db.session.add(new_employee)
-        db.session.commit()
+#         new_employee = Employee(name=name, state=state, dept_code=dept_code)
+#         db.session.add(new_employee)
+#         db.session.commit()
         
-        return redirect('/phones')
-    else:
-        return render_template('add_employee_form.html', form=form)
+#         return redirect('/phones')
+#     else:
+#         return render_template('add_employee_form.html', form=form)
 
-@app.route('/employees/<int:user_id>/edit', methods=['GET', 'POST'])
-def edit_user(user_id):
-    employee = Employee.query.get_or_404(user_id)
-    # obj=employee prepopulates fields automatically based on employee data
-    form = EmployeeForm(obj=employee)
+# @app.route('/employees/<int:user_id>/edit', methods=['GET', 'POST'])
+# def edit_user(user_id):
+#     employee = Employee.query.get_or_404(user_id)
+#     # obj=employee prepopulates fields automatically based on employee data
+#     form = EmployeeForm(obj=employee)
 
-    depts = db.session.query(Department.dept_code, Department.dept_name)
-    form.dept_code.choices = depts
+#     depts = db.session.query(Department.dept_code, Department.dept_name)
+#     form.dept_code.choices = depts
 
-    if form.validate_on_submit():
-        employee.name = form.name.data
-        employee.state = form.state.data
-        employee.dept_code = form.dept_code.data
+#     if form.validate_on_submit():
+#         employee.name = form.name.data
+#         employee.state = form.state.data
+#         employee.dept_code = form.dept_code.data
 
-        db.session.commit()
-        return redirect('/phones')
-    else:
-        return render_template('edit_employee_form.html', form=form)
+#         db.session.commit()
+#         return redirect('/phones')
+#     else:
+#         return render_template('edit_employee_form.html', form=form)
--- seed.py
+++ seed.py
@@ -7,28 +7,45 @@ from app import app
 db.drop_all()
 db.create_all()
 
-marketing = Department(dept_code='mktg', dept_name='Marketing', phone='610-2999')
-accounting = Department(dept_code='acct', dept_name='Account', phone='610-2932')
-sales = Department(dept_code='sales', dept_name='Sales')
+jerry = Pet(name="Jerry", 
+            species="Dog", 
+            photo_url="https://images.unsplash.com/photo-1583511655857-d19b40a7a54e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1949&q=80",
+            age = 10)
 
-billy = Employee(name='Billy Joel', state='MD', dept_code='mktg')
-buko = Employee(name='Buko Pie', state='NY', dept_code='mktg')
-bob = Employee(name='Bob Ross', state='PA', dept_code='acct')
-jim = Employee(name='Jim Halpert', state='PA', dept_code='sales')
-ryan = Employee(name='Ryan Reynalds', dept_code='sales')
-freelancer = Employee(name='Free Lancer')
+samantha = Pet(name="Samantha", 
+            species="Bird", 
+            photo_url="https://images.unsplash.com/photo-1551085254-e96b210db58a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1486&q=80",
+            age = 12)
 
-car = Project(proj_code='car', proj_name='Design Car')
-server = Project(proj_code='server', proj_name='Deploy Server')
+ricardo = Pet(name="Ricardo", 
+            species="Dog")
 
-billy_job_1 = EmployeeProject(emp_id=1, proj_code='car', role='Blueprints Car')
-billy_job_2 = EmployeeProject(emp_id=1, proj_code='server', role='Serves Ice Cream')
-buko_job = EmployeeProject(emp_id=2, proj_code='server', role='Serves Pizza')
+db.session.add_all([jerry, samantha, ricardo])
 
-db.session.add_all([marketing, accounting, sales])
-db.session.add_all([billy, buko, bob, jim, ryan, freelancer])
-db.session.add_all([car, server])
 db.session.commit()
 
-db.session.add_all([billy_job_1, billy_job_2, buko_job])
-db.session.commit()
+# marketing = Department(dept_code='mktg', dept_name='Marketing', phone='610-2999')
+# accounting = Department(dept_code='acct', dept_name='Account', phone='610-2932')
+# sales = Department(dept_code='sales', dept_name='Sales')
+
+# billy = Employee(name='Billy Joel', state='MD', dept_code='mktg')
+# buko = Employee(name='Buko Pie', state='NY', dept_code='mktg')
+# bob = Employee(name='Bob Ross', state='PA', dept_code='acct')
+# jim = Employee(name='Jim Halpert', state='PA', dept_code='sales')
+# ryan = Employee(name='Ryan Reynalds', dept_code='sales')
+# freelancer = Employee(name='Free Lancer')
+
+# car = Project(proj_code='car', proj_name='Design Car')
+# server = Project(proj_code='server', proj_name='Deploy Server')
+
+# billy_job_1 = EmployeeProject(emp_id=1, proj_code='car', role='Blueprints Car')
+# billy_job_2 = EmployeeProject(emp_id=1, proj_code='server', role='Serves Ice Cream')
+# buko_job = EmployeeProject(emp_id=2, proj_code='server', role='Serves Pizza')
+
+# db.session.add_all([marketing, accounting, sales])
+# db.session.add_all([billy, buko, bob, jim, ryan, freelancer])
+# db.session.add_all([car, server])
+# db.session.commit()
+
+# db.session.add_all([billy_job_1, billy_job_2, buko_job])
+# db.session.commit()
